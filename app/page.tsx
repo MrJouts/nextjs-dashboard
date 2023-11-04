0d21826cfb07d93c96659a66e7e0af2c6d307301
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-11-04 14:04:02 -0300

    fix - removed status and triangles

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-import styles from '@/app/ui/home.module.css';
-import clsx from 'clsx';
-
 import Link from 'next/link';
 export default function Page() {
 
@@ -17,22 +14,6 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-
-          <div
-            className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
-          />
-          <div className={styles.shape}></div>
-
-          <span
-            className={clsx(
-              'inline-flex items-center rounded-full px-2 py-1 text-sm',
-              {
-                'bg-gray-100 text-gray-500': status === 'pending',
-                'bg-green-500 text-white': status === 'paid',
-              },
-            )}
-          >Status</span>
-
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
